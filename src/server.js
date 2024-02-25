d3ecc24f9004d6807eef101cbdd4c3861bb3e997
Crist28 <cristianjaimes691@gmail.com>
2024-02-25 15:45:23 -0500

    vercel mongodb

---
+++ src/server.js
@@ -0,0 +1,30 @@
+//librerias
+const express = require('express');
+
+//llamado a otros documentos
+// const { dbConnection } = require('./config/database');
+
+class Server{
+
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT || 4100
+
+        //routers
+        this.routers() ;
+        
+    }
+    routers(){
+        this.app.get('/', function (req, res) {
+            res.send('Hello World');
+        });
+    }
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log("Servidor Corriendo en el Puerto:", this.port)
+        })
+    }
+}
+module.exports={
+    Server
+}
